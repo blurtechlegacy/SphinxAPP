950a02c22eb98117c0d8498cb1d822ed0e82189d
Rainor <Srg.Grch@gmail.com>
2018-05-13 05:53:05 +0700

    del c

--- app/src/main/java/com/syberianguys/srggrch/sphinx/MainActivity.java
+++ app/src/main/java/com/syberianguys/srggrch/sphinx/MainActivity.java
@@ -25,7 +25,6 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int c = 1;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 

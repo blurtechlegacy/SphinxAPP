73d448e9656b760d41532cb27a434d4c2061d45c
Rainor <Srg.Grch@gmail.com>
2018-05-13 12:37:28 +0700

    Indicator added

--- app/src/main/java/com/syberianguys/srggrch/sphinx/HLAdapter.java
+++ app/src/main/java/com/syberianguys/srggrch/sphinx/HLAdapter.java
@@ -1,13 +1,17 @@
 package com.syberianguys.srggrch.sphinx;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class HLAdapter extends ArrayAdapter<HLNames> {
     public HLAdapter( Context context, HLNames[] resource) {
@@ -23,6 +27,7 @@ public class HLAdapter extends ArrayAdapter<HLNames> {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.adapter_house, null);
         }
 
+        if (position == 5) ((ImageView) convertView.findViewById(R.id.houseIndicator)).setImageResource(R.drawable.color_error);
         // Заполняем адаптер
         ((TextView) convertView.findViewById(R.id.houseName)).setText(name.name);
         return convertView;
--- app/src/main/java/com/syberianguys/srggrch/sphinx/MainActivity.java
+++ app/src/main/java/com/syberianguys/srggrch/sphinx/MainActivity.java
@@ -2,8 +2,6 @@ package com.syberianguys.srggrch.sphinx;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -15,6 +13,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -23,33 +22,25 @@ public class MainActivity extends AppCompatActivity
 
     private static final String LOG_TAG = "MainActivity";
     ListView mList;
+    Button ind;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         //=======================================================================================\\
 
-        mList = findViewById(R.id.mList);
+        mList = findViewById(R.id.hList);
         mList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -58,9 +49,8 @@ public class MainActivity extends AppCompatActivity
                         Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this, FlatListActivity.class);
                 intent.putExtra("position", position);
-                Log.d(LOG_TAG, "before start");
+                Log.d(LOG_TAG, "CLICKED");
                 startActivity(intent);
-                //передать id тыкнутого элемента в FL
 
             }
         });
@@ -70,7 +60,7 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
         } else {
@@ -129,7 +119,7 @@ public class MainActivity extends AppCompatActivity
 
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
